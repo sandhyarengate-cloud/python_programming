3173fa82a1a999f85776acaccd84c3248f08e11f
sandhyarengate-cloud <sandhyarengate@gmail.com>
2026-03-27 09:05:48 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,53 @@
+#a=int(input("enter a first number:"))
+#b=int(input("enter a second number:"))
+#c=int(input("enter a third number:"))
+#if a<b:
+ #   if a<c:
+  #      print("a is min")
+   # else:
+    #    print("c is  min")
+
+#else:
+ #   if b<c:
+  #      print("b is min")
+   # else:
+    #    print("c is min")
+#mylist=[3,5,6,8,2]
+#for i in mylist:
+    #print(i)
+#for i in range(1,11):
+   #print(i*2," ",i*3," ",i*4," ",i*5," ",i*6," ",i*7," ",i*8," ",i*9," ",i*10," ",i*11," ",i*12," ",i*13)
+#username=input("enter username:")
+#password=input("enter password:")
+#if username==password:
+ #   print("login successful")
+#else:
+  #  print("invalid login")
+#brand=input("enter your coldrink name either in upper case or in lower case:")
+#if brand =="pepsi" or brand=="PEPSI":
+    #print("swag")
+#elif brand=="dew" or brand=="DEW":
+   # print("dar age jeet hai")
+##elif brand=="thumsup" or brand=="THUMSUP":
+ #   print("taste the thunder")
+#else:
+#print("go with your brand")
+#n1=int(input("enter first number:"))
+#n2=int(input("enter second number:"))
+#n3=int(input("enter third number:"))
+#if n1>n2 and n1>n3:
+   # print("biggest number is:",n1)
+#elif n2>n3:
+   # print("biggest number is:",n2)
+#else:
+   # print("biggest number is:",n3)
+count=0
+for i in range(9):
+    if i%2==0:
+        print(i)
+    else:
+        print(i)
+    count+=1
+print("count=",count)
+
+
