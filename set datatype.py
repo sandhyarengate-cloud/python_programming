621b317f985b6f9c1bd138bb20a6c7f23fa19153
sandhyarengate-cloud <sandhyarengate@gmail.com>
2026-03-27 09:04:19 +0530

    Add files via upload

---
+++ set datatype.py
@@ -0,0 +1,17 @@
+##print(myset)
+#print(type(myset))
+#myset.add(60)
+#print(myset)
+#myset.remove(3)
+#print(myset)
+#myset={10,20,30,40}
+#youset={"prashant","jha"}
+#newset=myset.union(youset)
+#print(newset)
+#intersection return common element
+#difference()method this will return the element
+#myset={10,20,30,40}
+#yorset={10,50,60,30}
+#print(myset.difference(yorset))
+#myset={10,20,30,40}
+#print(myset.clear())
