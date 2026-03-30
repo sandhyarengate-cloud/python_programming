0f23d252616a608da656a07e0e33abb7af05dcc7
sandhyarengate-cloud <sandhyarengate@gmail.com>
2026-03-30 12:54:11 +0530

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,48 @@
+# list=[1,4,7,9,0,2]
+# list.sort()
+# print(list)
+# print(list[5])
+# list=[0,1,0,3,12]
+# list.sort(reverse=True)
+# print(list)
+# list=[0,1,0,3,12]
+# for i in list:
+#     if i==0:
+#         list.remove(i)
+#         list.append(i)
+# print(list)
+# A=[1,2,3]
+# B=[2,3,4]
+# C=[3,4,5]
+# for i in A:
+#     if i in B and in C:
+#         print(i)
+#while loop syntax
+# i=1
+# while i<=5:
+#     print(i)
+#
+
+
+
+# sum=0
+# i=1
+# while i<=10:
+#     sum = sum +i
+#     i = i+1
+# print("Sum",sum)
+
+# mul=1
+# i=1
+# while i<=5:
+#     mul=mul*i
+#     print(mul)
+#     i=i+1
+# for i,j in zip(range(1,6),range(5,0,-1)):
+#     if i == 3 and j == 3:
+#         continue
+#     print(i," ",j)
+
+#NESTED FOR LOOP
+# Write your code here :-)
+# Write your code here :-)
